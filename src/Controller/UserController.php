c00d692f41a78fdc2592941e41110c68980e3207
Brian Didierjean <contact@briandidierjean.dev>
2021-01-18 21:23:09 +0100

    core: Add user controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("sign-in", name="user_sign_in")
+     */
+    public function signIn(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/sign-in.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    /**
+     * @Route("sign-out", name="user_sign_out")
+     */
+    public function signOut(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+}
