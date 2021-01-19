82e558c959bc336b4275e0016c24a640891a354d
Brian Didierjean <contact@briandidierjean.dev>
2021-01-19 15:49:24 +0100

    core: Add reset password type

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Nouveau mot de passe : '],
+                'second_options' => ['label' => 'Nouveau mot de passe (confirmation) : '],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
