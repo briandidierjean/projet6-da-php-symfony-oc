e9c6b7928b6954da9c1ad169c37952a073250b2b
Brian Didierjean <contact@briandidierjean.dev>
2021-02-08 14:19:12 +0100

    feat: Add group trick add view

--- src/Controller/TrickGroupController.php
+++ src/Controller/TrickGroupController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 
 
 use App\Entity\TrickGroup;
+use App\Form\TrickGroupType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickGroupController extends AbstractController
@@ -13,8 +16,22 @@ class TrickGroupController extends AbstractController
     /**
      * @Route("add-group-trick", name="group_trick_add")
      */
-    public function add(): Response
+    public function add(Request $request): Response
     {
         $trickGroup = new TrickGroup();
+
+        $form = $this->createForm(TrickGroupType::class, $trickGroup);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($trickGroup);
+            $entityManager->flush();
+        }
+
+        return $this->render('trick-group/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
