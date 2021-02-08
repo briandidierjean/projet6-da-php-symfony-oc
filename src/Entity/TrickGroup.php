f11b2c2643c72df4147c6093d6790f015ccccf30
Brian Didierjean <contact@briandidierjean.dev>
2021-02-08 13:55:27 +0100

    feat: Add Trick Group entity

---
+++ src/Entity/TrickGroup.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table("trick_groups")
+ * @UniqueEntity(
+ *     fields="name",
+ *     message="Ce nom est déjà utilisé."
+ * )
+ */
+class TrickGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string" unique=true)
+     * @Assert\NotBlank(
+     *     message="Veuillez saisir un nom."
+     * )
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
